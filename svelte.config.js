08d02be5c4492d166228b8b4a19e34218e0ddc7f
Thomas Bailey <thomas@noisemachines.io>
2022-01-09 00:22:47 -0500

    Tweak esbuild options.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare'
+import cloudflare from '@sveltejs/adapter-cloudflare'
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,9 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter(),
+		adapter: cloudflare({
+			platform: 'node'
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
